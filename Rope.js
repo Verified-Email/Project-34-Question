e69c218165c49ed809164f05bebc148a5c6520aa
Verified-Email <71946266+Verified-Email@users.noreply.github.com>
2021-01-14 17:10:00 -0600

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,33 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.01,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        if(this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            stroke("brown");
+            strokeWeight(5);
+            line(pointA.x, pointA.y , pointB.x, pointB.y);
+            pop();
+        }
+    }
+    
+    fly(){
+        this.chain.bodyA = null;
+    }
+
+    attach(body){
+        this.chain.bodyA = body;
+    }
+}
